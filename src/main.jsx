4b9d9686226dd42e8c215b985cebb0a23d4ab7cc
Sadat <manikagarg0000@gmail.com>
2026-03-05 22:04:43 +0600

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './opnet-launchpad'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
